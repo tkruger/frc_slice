5a4172ad5374f6e9bc5b6ed7b4fa903024ec97f1
flightpath24 <69678154+flightpath24@users.noreply.github.com>
2022-07-25 14:46:11 -0400

    Added minimum shooter code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,4 +24,8 @@ public final class Constants {
     public static final int drivetrain_RIGHT_FRONT_PORT = 1;
     public static final int drivetrain_RIGHT_BACK_PORT = 2;
 
+    //Define Shooter Motor Ports
+    public static final int intake_FLYWHEEL_PRIMARY_PORT = 5;
+    public static final int intake_FLYWHEEL_SECONDARY_PORT = 6;
+
 }
