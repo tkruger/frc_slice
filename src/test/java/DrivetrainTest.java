d81f6f10f53ff446b462b5fd9b2a3b8c58a72436
flightpath24 <evan.p.krug@gmail.com>
2022-09-02 13:13:49 -0400

    Changed method of passing joysticks, add encoders

--- src/test/java/DrivetrainTest.java
+++ src/test/java/DrivetrainTest.java
@@ -5,13 +5,14 @@ import edu.wpi.first.wpilibj.simulation.PWMSim;
 import org.junit.*;
 
 public class DrivetrainTest {
+    // TODO: Use CAN declarations
     PWMSim simMotor;
     public static final double DELTA = 1e-2; // acceptable deviation range
 
     @Before // this method will run before each test
     public void setup() {
         assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
-        // TODO: Fix port
+        
         simMotor = new PWMSim(0); // create our simulation PWM motor controller
     }
 
@@ -24,7 +25,6 @@ public class DrivetrainTest {
         /**
          * TODO: Implement motor tests
          */
-
 
         // Act as if a user is pushing the forward button
         // Assert in case the motor is NOT doing what you expect
