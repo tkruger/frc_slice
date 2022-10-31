2b17a63cb304fe775439a2353b63fab1e77cbf7c
Thomas <tlhart365@gmail.com>
2022-10-31 14:42:34 -0400

    Lowered max speed and acceleration on pathplanner

--- src/test/java/DrivetrainTest.java
+++ src/test/java/DrivetrainTest.java
@@ -11,11 +11,10 @@ import org.junit.*;
 public class DrivetrainTest {
     PWMSim simMotor;
     public static final double DELTA = 1e-2; // acceptable deviation range
-    
+
     //Define Joysticks
     public static JoystickSim leftJoystick = new JoystickSim(Constants.RobotContainer_LEFT_JOYSTICK_PORT);
     public static JoystickSim rightJoystick = new JoystickSim(Constants.RobotContainer_RIGHT_JOYSTICK_PORT);
-
 
     @Before // this method will run before each test
     public void setup() {
