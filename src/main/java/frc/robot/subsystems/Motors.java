93d73d2c4f30bbbf8f118401046b7a2a1e5f70c6
RomanMartin100 <112425985+RomanMartin100@users.noreply.github.com>
2022-11-15 14:40:03 -0500

    Files for yaw and pitch motors
    Imported the file for the Motors subsystem, which controls the yaw and pitch motors.

---
+++ src/main/java/frc/robot/subsystems/Motors.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/* The yaw and pitch motor will rotate the cannon a full 360 degrees
+ * and will lift it from a 15 degree angle to a 75 degree angle */
+
+public class Motors extends SubsystemBase {
+  //Declaring the motor variables
+  TalonFX yawMotor;
+  TalonFXConfiguration yawMotor_Config;
+  CANSparkMax pitchMotor;
+
+  /** Creates a new Motors Subsystem (for the yaw and pitch motor). */
+  public Motors() {
+    //Initiating Motors
+    yawMotor = new TalonFX(0);
+    yawMotor_Config = new TalonFXConfiguration();
+
+    pitchMotor = new CANSparkMax(1, MotorType.kBrushless);
+  }
+
+  //This command will run both the yaw and pitch motors
+  public void runMotors(){
+    
+    
+
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
