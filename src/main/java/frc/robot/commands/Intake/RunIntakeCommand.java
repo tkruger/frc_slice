0feee01cd5960dc87eaea8572a2b5d16398926c2
MikeVey42 <steelege07@gmail.com>
2023-02-21 10:42:20 -0500

    Run Intake Command

---
+++ src/main/java/frc/robot/commands/Intake/RunIntakeCommand.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+
+public class RunIntakeCommand extends CommandBase {
+  private final Intake m_intake;
+  private final double m_speed;
+  /** Creates a new RunIntakeCommand. */
+  public RunIntakeCommand(Intake intake, double speed) {
+    m_intake = intake;
+    m_speed = speed;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_intake.runIntake(m_speed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
