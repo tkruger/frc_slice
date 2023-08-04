79f8d1de145fadb535643eed96b7397093432e59
MikeVey42 <steelege07@gmail.com>
2023-08-04 14:55:27 -0400

    PID Tuner Widget

---
+++ src/main/java/frc/robot/ShuffleboardPIDWidget.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.Map;
+
+import com.revrobotics.SparkMaxPIDController;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+/** 
+ * This represents a custom widget on shuffleboard that accepts a P, I, and D hain
+ * and updates the provided PID controler to those gains when a button in the widget is pressed
+ */
+public class ShuffleboardPIDWidget {
+    private ShuffleboardTab tab;
+    private ShuffleboardLayout list;
+
+    private SimpleWidget pGainWidget, iGainWidget, dGainWidget;
+
+    private SparkMaxPIDController pidController;
+
+    public ShuffleboardPIDWidget(String name, SparkMaxPIDController pidController) {
+        this.pidController = pidController;
+
+        tab = Shuffleboard.getTab("PID Tuning");
+        // A grid layout is used here rather than a list in order to control the order of the items
+        list = tab.getLayout(name, BuiltInLayouts.kGrid)
+            .withProperties(Map.of("Number of columns", 1, "Number of rows", 4));
+    
+        pGainWidget = list.add("P", 0.0)
+            .withPosition(0, 0);
+
+        iGainWidget = list.add("I", 0.0)
+            .withPosition(0, 1);
+
+        dGainWidget = list.add("D", 0.0)
+            .withPosition(0, 2);
+
+        list.add("Update", new InstantCommand(this::updateGains))
+            .withPosition(0, 3)
+            .withWidget(BuiltInWidgets.kCommand);
+    }
+
+    public void updateGains() {
+        pidController.setP(pGainWidget.getEntry().getDouble(0.0));
+        pidController.setI(iGainWidget.getEntry().getDouble(0.0));
+        pidController.setP(dGainWidget.getEntry().getDouble(0.0));
+    }
+}
